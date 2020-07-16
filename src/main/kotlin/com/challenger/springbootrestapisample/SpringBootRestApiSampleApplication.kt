43a3f2995893d6020971bcfe463866d45d57dc0e
André dos Santos <adesantosa@gmail.com>
2020-07-16 15:41:57 -0300

    first commit

---
+++ src/main/kotlin/com/challenger/springbootrestapisample/SpringBootRestApiSampleApplication.kt
@@ -0,0 +1,11 @@
+package com.challenger.springbootrestapisample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootRestApiSampleApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringBootRestApiSampleApplication>(*args)
+}
---
+++ src/test/kotlin/com/challenger/springbootrestapisample/SpringBootRestApiSampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.challenger.springbootrestapisample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootRestApiSampleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
